42a30109e677f3dcf6ef446621374a92f7b81908
Oktawian Kausz <36113911+whycody@users.noreply.github.com>
2020-10-15 15:25:34 +0200

    Changed accent color and added animation to circle in Startup fragment

--- app/src/main/java/com/whycody/catshealth/startup/StartupFragment.kt
+++ app/src/main/java/com/whycody/catshealth/startup/StartupFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.whycody.catshealth.R
+import kotlinx.android.synthetic.main.fragment_startup.view.*
 
 class StartupFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_startup, container, false)
+        view.circleLogo.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.circle_logo_anim))
         return view
     }
 }
