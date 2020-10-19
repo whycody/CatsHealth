71c2de988ab94cffad562eda9e9704fda4ab642e
Oktawian Kausz <36113911+whycody@users.noreply.github.com>
2020-10-19 19:59:44 +0200

    Added BottomSheet panel with information about number of selected symptoms

--- app/src/main/java/com/whycody/catshealth/symptoms/SymptomsFragment.kt
+++ app/src/main/java/com/whycody/catshealth/symptoms/SymptomsFragment.kt
@@ -6,34 +6,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.whycody.catshealth.MainActivity
 import com.whycody.catshealth.R
 import com.whycody.catshealth.data.SymptomItem
+import com.whycody.catshealth.databinding.FragmentSymptomsBinding
 import com.whycody.catshealth.symptoms.recycler.SymptomAdapter
 import kotlinx.android.synthetic.main.fragment_symptoms.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SymptomsFragment : Fragment() {
 
+    private lateinit var symptomsBottomSheetBeh: BottomSheetBehavior<LinearLayout>
     private val symptomsViewModel: SymptomsViewModel by viewModel()
+    private var checkedSymptomItems = listOf<SymptomItem>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_symptoms, container, false)
+        val binding: FragmentSymptomsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_symptoms, container, false)
         val adapter = SymptomAdapter(symptomsViewModel)
-        view.symptomsRecycler.layoutManager = LinearLayoutManager(activity?.applicationContext)
-        view.symptomsRecycler.adapter = adapter
-        loadLayoutAnimation(view.symptomsRecycler)
-        observeSymptoms(adapter)
-        return view
+        symptomsBottomSheetBeh = BottomSheetBehavior.from(binding.symptomsBottomSheet)
+        symptomsBottomSheetBeh.state = BottomSheetBehavior.STATE_HIDDEN
+        binding.symptomsRecycler.layoutManager = LinearLayoutManager(activity?.applicationContext)
+        binding.symptomsRecycler.adapter = adapter
+        loadLayoutAnimation(binding.symptomsRecycler)
+        observeSymptoms(adapter, binding)
+        return binding.root
     }
 
-    private fun observeSymptoms(adapter: SymptomAdapter) {
+    private fun observeSymptoms(adapter: SymptomAdapter, binding: FragmentSymptomsBinding) {
         symptomsViewModel.getSymptomsItems().observe(activity as MainActivity, {
+            refreshBinding(binding, it)
             refreshAdapter(adapter, it)
+            refreshBottomSheetState()
         })
+    }
+
+    private fun refreshBinding(binding: FragmentSymptomsBinding, symptomsItemsList: List<SymptomItem>) {
+        checkedSymptomItems = symptomsItemsList.filter { symptomItem -> symptomItem.checked }
+        binding.checkedSymptomsSize = checkedSymptomItems.size
+    }
+
+    private fun refreshBottomSheetState() {
+        symptomsBottomSheetBeh.state =
+            if(checkedSymptomItems.isNotEmpty())
+                BottomSheetBehavior.STATE_EXPANDED
+            else BottomSheetBehavior.STATE_HIDDEN
     }
 
     private fun refreshAdapter(adapter: SymptomAdapter, symptomsItemsList: List<SymptomItem>) {
