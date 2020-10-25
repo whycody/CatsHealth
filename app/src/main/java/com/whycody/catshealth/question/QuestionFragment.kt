3e6aaf5ddb3778f534be700711f153f8b0673ed3
Oktawian Kausz <36113911+whycody@users.noreply.github.com>
2020-10-25 16:57:30 +0100

    Added animations to QuestionFragment

--- app/src/main/java/com/whycody/catshealth/question/QuestionFragment.kt
+++ app/src/main/java/com/whycody/catshealth/question/QuestionFragment.kt
@@ -5,23 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
+import com.whycody.catshealth.MainActivity
 import com.whycody.catshealth.R
 import com.whycody.catshealth.databinding.FragmentQuestionBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(), Animation.AnimationListener {
 
     private val questionViewModel: QuestionViewModel by viewModel()
+    private lateinit var headerQuestion: LinearLayout
+    private lateinit var questionCircle: ImageView
+    private lateinit var fadeOutAnim: Animation
+    private lateinit var fadeInAnim: Animation
+    private lateinit var circleQuestionAnim: Animation
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentQuestionBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_question, container, false)
+        setupAnimations()
         binding.questionViewModel = questionViewModel
         binding.lifecycleOwner = activity
+        headerQuestion = binding.headerQuestion
+        questionCircle = binding.questionCircle
+        headerQuestion.startAnimation(fadeInAnim)
+        questionCircle.startAnimation(circleQuestionAnim)
+        observeClickedBtnIndex()
         return binding.root
     }
+
+    private fun observeClickedBtnIndex() {
+        questionViewModel.getClickedBtnIndex().observe(activity as MainActivity, {
+            if(it == -1) return@observe
+            headerQuestion.startAnimation(fadeOutAnim)
+        })
+    }
+
+    private fun setupAnimations() {
+        fadeOutAnim = AnimationUtils.loadAnimation(activity, R.anim.fade_out)
+        fadeInAnim = AnimationUtils.loadAnimation(activity, R.anim.fade_in)
+        circleQuestionAnim = AnimationUtils.loadAnimation(context, R.anim.circle_question_anim)
+        fadeOutAnim.setAnimationListener(this)
+    }
+
+    override fun onAnimationStart(animation: Animation?) { }
+
+    override fun onAnimationEnd(animation: Animation?) {
+        questionViewModel.setupQuestion()
+        headerQuestion.startAnimation(fadeInAnim)
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) { }
 
     companion object {
         const val SYMPTOM_QUESTION = 0
--- app/src/main/java/com/whycody/catshealth/question/QuestionViewModel.kt
+++ app/src/main/java/com/whycody/catshealth/question/QuestionViewModel.kt
@@ -1,6 +1,7 @@
 package com.whycody.catshealth.question
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.whycody.catshealth.data.SearchResult
@@ -10,11 +11,12 @@ class QuestionViewModel(private val searchResult: SearchResult,
                         private val searchDiseaseUtil: SearchDiseaseUtil): ViewModel() {
 
     var question: MutableLiveData<String> = MutableLiveData(searchResult.currentQuestion?.question)
+    var clickedBtnIndex: MutableLiveData<Int> = MutableLiveData(-1)
 
     fun btnClicked(noticed: Boolean) {
         setSearchResultVariables(noticed)
         searchDiseaseUtil.refreshSearchResult(searchResult)
-        setupQuestion()
+        clickedBtnIndex.value = if(noticed) 0 else 1
     }
 
     private fun setSearchResultVariables(noticed: Boolean) {
@@ -26,12 +28,17 @@ class QuestionViewModel(private val searchResult: SearchResult,
             searchResult.alreadyAskedDiseasesQuestionsIds.add(currentQuestion!!.askingObjectId)
     }
 
-    private fun setupQuestion() {
+    fun setupQuestion() {
         question.value = "No more questions"
+        clickedBtnIndex.value = -1
         if(searchResult.currentQuestion != null)
             question.value = searchResult.currentQuestion?.question
         else if(searchResult.currentQuestion == null && searchResult.probableDiseaseId != null)
             Log.d("MOJTAG", "Probable disease: ${searchResult.probableDiseaseId}")
         else Log.d("MOJTAG", "Could not find probable disease")
     }
+
+    fun getQuestion(): LiveData<String> = question
+
+    fun getClickedBtnIndex(): LiveData<Int> = clickedBtnIndex
 }
